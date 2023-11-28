d8ed1e269d5cf54884e0214501cdbd99d63a9cbf
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-28 12:09:23 +0100

    Inicio del proyecto

---
+++ src/mvc_EventoBoton/MVC_EventoBoton.java
@@ -0,0 +1,11 @@
+package mvc_EventoBoton;
+
+public class MVC_EventoBoton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Iniciando programa ...");
+
+	}
+
+}
