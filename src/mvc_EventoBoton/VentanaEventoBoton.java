d4f1f171f81da2d32975ede7a37eb2643bcd9e9b
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-28 12:31:41 +0100

    Coloco un botón

--- src/mvc_EventoBoton/VentanaEventoBoton.java
+++ src/mvc_EventoBoton/VentanaEventoBoton.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class VentanaEventoBoton extends JFrame {
 
@@ -16,11 +18,15 @@ public class VentanaEventoBoton extends JFrame {
 	 */
 	public VentanaEventoBoton() {
 		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 260, 130);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton bBoton = new JButton("Di algo !!!");
+		bBoton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		contentPane.add(bBoton, BorderLayout.CENTER);
 	}
 
 }
